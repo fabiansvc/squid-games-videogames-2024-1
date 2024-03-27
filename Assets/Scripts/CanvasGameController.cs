2de32d607151aef950d13e16ac932283cb8bdfcb
Fabian Valencia <fabian.cordoba@correounivalle.edu.co>
2024-03-26 19:03:37 -0500

    Sesion 6 implemented 20% where user can start the game and move around the world

--- Assets/Scripts/CanvasGameController.cs
+++ Assets/Scripts/CanvasGameController.cs
@@ -4,8 +4,15 @@ using UnityEngine;
 
 public class CanvasGameController : MonoBehaviour
 {
+    private static CanvasGameController instance;
     public Canvas CanvasGame;
     public Canvas CanvasWelcome;
+
+    void Awake()
+    {
+        instance = this;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,12 +22,18 @@ public class CanvasGameController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     public void B_OnHandleButtonExit()
     {
         CanvasGame.gameObject.SetActive(false);
         CanvasWelcome.gameObject.SetActive(true);
+        Game.GetInstance().SetIsStarted(false);
+    }
+
+    public static CanvasGameController GetInstance()
+    {
+        return instance == null ? instance = new CanvasGameController() : instance;
     }
 }
