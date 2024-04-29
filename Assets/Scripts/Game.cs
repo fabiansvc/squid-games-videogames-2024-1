2d55e3d4a4a948cc7dd5a1e0d4af6b3a3475ef95
Fabian Stiven Valencia Cordoba <fabian.cordoba@correounivalle.edu.co>
2024-04-29 11:15:55 -0500

    Sesion 9 start

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -6,7 +6,6 @@ using TMPro;
 public class Game : MonoBehaviour
 {
     private static Game instance;
-    private float speed = 2f;
     private bool isStarted = false;
     public GameObject TextTime;
     public TMP_Text TmpText;
@@ -16,11 +15,6 @@ public class Game : MonoBehaviour
     void Awake()
     {
         instance = this;
-    }
-    // Start is called before the first frame update
-    void Start()
-    {
-        
     }
 
     // Update is called once per frame
@@ -34,21 +28,7 @@ public class Game : MonoBehaviour
             if (time == 0)
             {
                 TextTime.SetActive(false);
-                float horizontalInput = Input.GetAxis("Horizontal");
-                float verticalInput = Input.GetAxis("Vertical");
-
-                Vector3 moveDirection = new Vector3(horizontalInput, 0, verticalInput).normalized;
-                transform.Translate(moveDirection * speed * Time.deltaTime);
-
-                float mouseX = Input.GetAxis("Mouse X");
-                transform.Rotate(Vector3.up * mouseX);
             }
-        }
-
-        if(Input.GetKeyDown(KeyCode.Escape) && enumerator != null)
-        {
-            Debug.Log("Stopping coroutine");
-            StopCoroutine(enumerator);
         }
     }
 
@@ -57,11 +37,9 @@ public class Game : MonoBehaviour
         time = 3;
         do
         {
-            Debug.Log(time.ToString() + " seconds left.");
             yield return new WaitForSeconds(1f);
             time--;
         }while(time>0);
-        Debug.Log("Start");
     }
 
     public void SetIsStarted(bool isStarted)
--- Assets/Scripts/RedMen/RedManTriangle.cs
+++ Assets/Scripts/RedMen/RedManTriangle.cs
@@ -7,12 +7,6 @@ public class RedManTriangle : MonoBehaviour
     public Transform RedManTriangleTransform;
     private float speed = 2.0f;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
